17c1519fa79bb6dd26033ea981ffbe530ef1e9c7
ERICK <ERICK@DESKTOP-DL3BQBQ>
2023-03-09 09:07:38 +0100

    commit uno

---
+++ src/marzo/Marzo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package marzo;
+
+/**
+ *
+ * @author ERICK
+ */
+public class Marzo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("erick nicolas");
+        System.out.println("hoy es dia 9 del 03 del 2023");
+    }
+    
+}
